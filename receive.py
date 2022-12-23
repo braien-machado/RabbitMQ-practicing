2e31df0aeebc58fe089dad193c6a3b33d7508254
Braien Machado de Paula <braienmp@outlook.com>
2022-12-23 14:53:07 -0300

    feat: improve producer and consumer - message acknowledgment - set queue as durable

--- receive.py
+++ receive.py
@@ -1,20 +1,21 @@
 import pika, sys, os
+from time import sleep
 
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='queue', durable=True)
 
 
-    def callback(channel, method, properties, body):
-        print(f' [x] Received {str(body)}')
+    def callback(channel, method, properties, body: bytes):
+        print(f' [x] Received {body.decode()}')
+        sleep(body.count(b'.'))
+        print(' [x] Done')
+        channel.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(
-        queue='hello',
-        auto_ack=True,
-        on_message_callback=callback
-    )
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- send.py
+++ send.py
@@ -1,16 +1,16 @@
-import pika
-
+import pika, sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='queue', durable=True)
 
-message = 'Hello, World!'
+message = ' '.join(sys.argv[1:]) or 'Hello World!'
 
 channel.basic_publish(
     exchange='',
-    routing_key='hello',
-    body=message
+    routing_key='queue',
+    body=message,
+    properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)
 )
 
 print(f' [x] Sent {message}')
